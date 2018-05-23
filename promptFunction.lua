e317ce0c65e5f83bd363058a49731d9d23b162dd
Joel Schumacher <joelschum@gmail.com>
2018-05-23 20:17:44 +0200

    Make promptFunction helper pass arguments to target command

--- promptFunction.lua
+++ promptFunction.lua
@@ -3,15 +3,20 @@ local commands = require("commands")
 local inputcommands = require("inputcommands")
 
 local function promptFunction(title, command, inputArgument, promptCommand, arguments)
-    local func = function(text)
-        input.toggle({{
+    local func = function(args)
+        local entry = {
             caption = "> " .. title,
             command = command,
             arguments = arguments or {},
-        }}, text, inputArgument)
+        }
+        -- add additional arguments to entry command arguments
+        for k, v in pairs(args) do
+            entry.arguments[k] = v
+        end
+        input.toggle({entry}, args.text, inputArgument)
     end
     local promptCommand = promptCommand or command .. "prompt"
-    commands.register(promptCommand, commands.wrap(func, {"text"}))
+    commands.register(promptCommand, func)
     inputcommands.register(title, promptCommand)
     return func
 end
